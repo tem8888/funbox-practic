c37a8bdcd6569a68e32467b3d4301b441d570952
tem <tem.8888@yandex.ru>
2020-08-28 22:46:07 +0300

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import './styles/style.sass'
+import React from 'react'
+import {render} from 'react-dom'
+import Content from './Content.jsx'
+
+render(
+	<Content />, document.getElementById('app'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: {
+		main: './src/index.jsx'
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname,'public')
+	},
+	devServer: {
+		port: 4444
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'src/favicon.ico'),
+					to: path.resolve(__dirname, 'public')
+				}
+			]
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader']
+			},
+			{
+				test: /\.sass$/,
+				use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+			},
+			{
+				test: /\.(png|jpg|jpeg|svg)$/,
+				use: ['file-loader']
+			},
+			{
+				test: /\.(otf|ttf|woff|woff2|eot)$/,
+				use: ['file-loader']
+			},
+			{
+				test: /\.jsx$/,
+				loader: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							'@babel/preset-env',
+							'@babel/preset-react'
+						]
+					}
+				}
+			}
+		]
+	}
+}
